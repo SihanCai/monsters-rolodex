02a9e0bdab5b8b2c5b073a63d7af6aa9ca17301e
Eric Cai <ericcai@amazon.com>
2020-05-18 17:26:32 -0500

    Updates

---
+++ precache-manifest.0ff06e3d957b8aebec3d22b322249e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9de05f0398f449366432338e2b8ff0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84cb7b73319a18e76ff7",
+    "url": "/monsters-rolodex/static/css/main.5aa5895b.chunk.css"
+  },
+  {
+    "revision": "7119da3d379c4695bcf5",
+    "url": "/monsters-rolodex/static/js/2.ffd80eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.ffd80eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cb7b73319a18e76ff7",
+    "url": "/monsters-rolodex/static/js/main.fcb189eb.chunk.js"
+  },
+  {
+    "revision": "be227f2eee6b8f69a1b4",
+    "url": "/monsters-rolodex/static/js/runtime-main.956250a9.js"
+  }
+]);
